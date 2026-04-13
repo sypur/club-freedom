dfed89e02a8a436123b45ea3fa43afb9f5a28842
Viet-Hung Nguyen <41867185+viethung7899@users.noreply.github.com>
2026-04-12 23:13:02 -0700

    Use empty components for loading and empty states

--- components/admin/organization-list.tsx
+++ components/admin/organization-list.tsx
@@ -1,9 +1,16 @@
 import { convexQuery } from "@convex-dev/react-query";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
-import { AlertCircle } from "lucide-react";
+import { AlertCircle, Building } from "lucide-react";
 import { api } from "@/convex/_generated/api";
 import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "../ui/empty";
 import { Spinner } from "../ui/spinner";
 
 export default function OrganizationList() {
@@ -13,27 +20,43 @@ export default function OrganizationList() {
 
   if (isLoading) {
     return (
-      <div className="flex flex-col gap-3 items-center p-4">
-        <Spinner />
-        <span>Loading...</span>
-      </div>
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia>
+            <Spinner />
+          </EmptyMedia>
+          <EmptyTitle>Loading...</EmptyTitle>
+        </EmptyHeader>
+      </Empty>
     );
   }
 
   if (error || !data) {
     return (
-      <div className="flex flex-col items-center gap-3 text-destructive p-4">
-        <AlertCircle />
-        <span>{error?.message || "An error occurred"}</span>
-      </div>
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <AlertCircle />
+          </EmptyMedia>
+          <EmptyTitle>An error occurred</EmptyTitle>
+          {error?.message && (
+            <EmptyDescription>{error.message}</EmptyDescription>
+          )}
+        </EmptyHeader>
+      </Empty>
     );
   }
 
   if (data.length === 0) {
     return (
-      <div className="flex flex-col items-center gap-4 p-4 bg-muted rounded-md">
-        No organizations
-      </div>
+      <Empty className="border border-dashed">
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <Building />
+          </EmptyMedia>
+          <EmptyTitle>No organizations</EmptyTitle>
+        </EmptyHeader>
+      </Empty>
     );
   }
 
