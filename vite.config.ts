4b0bc34a27034dd13de60926da87c09e371b0c86
Nicholas Tsang <nicktsang.manhay@gmail.com>
2026-06-17 20:58:56 -0700

    Revert "Remove devtools in production"

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
-import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
@@ -32,7 +31,6 @@ export default defineConfig(({ mode, command }) => {
       },
     },
     plugins: [
-      devtools(),
       cloudflare({ viteEnvironment: { name: "ssr" } }),
       tailwindcss(),
       tanstackStart({
