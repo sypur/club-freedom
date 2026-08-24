b89bf159b4b0181e4aa2e1f03595688be44ac50c
Viet-Hung Nguyen <41867185+viethung7899@users.noreply.github.com>
2026-08-23 17:57:40 -0700

    New file for calculate the schedule time

--- convex/utils.ts
+++ convex/utils.ts
@@ -1,43 +1,7 @@
-import { TZDate } from "@date-fns/tz";
-import { addDays } from "date-fns";
-
 export function removeUndefinedFromRecord<T extends Record<string, unknown>>(
   record: T,
 ) {
   return Object.fromEntries(
     Object.entries(record).filter(([_, value]) => value !== undefined),
   ) as Partial<T>;
-}
-
-export function getNextScheduleTime(
-  allowedDaysOfTheWeek: number[],
-  hour: number,
-  timezone?: string | null,
-): Date {
-  const targetTz = timezone || Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const now = Date.now();
-  const nowInTz = new TZDate(new Date(), targetTz);
-
-  const target = new TZDate(
-    nowInTz.getFullYear(),
-    nowInTz.getMonth(),
-    nowInTz.getDate(),
-    hour,
-    0,
-    0,
-    0,
-    targetTz,
-  );
-
-  for (let i = 0; i < 8; i++) {
-    const targetDay = target.getDay();
-
-    if (allowedDaysOfTheWeek.includes(targetDay) && target.getTime() > now) {
-      return target;
-    }
-
-    target.setDate(target.getDate() + 1);
-  }
-
-  return new Date(target.getTime());
 }
